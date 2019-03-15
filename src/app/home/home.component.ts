a02008dbca3f626d9083b912d03bdaee6792e98d
ahmedaladdin6 <ahmedaladdin6@gmail.com>
2019-03-15 22:58:47 +0200

    delete button done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output ,EventEmitter, DoCheck} from '@angular/core';
+import { Notes } from '../class/notes';
 
 
 
@@ -8,23 +9,29 @@ import { Component, OnInit, Output ,EventEmitter, DoCheck} from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit ,DoCheck{
-   arrOfNotes=[];
-
+   arrOfNotes: Notes[]=[ ];
+   loadedfeature ='home';
    numberOfNotes:number;
+  
+  
 
   constructor() { }
   ngOnInit() { }
-  loadedfeature ='home';
+  
   onNavigate(feature:string){
      this.loadedfeature = feature;
   }
 
   addNote(note:{name:string ,content:string}){
- this.arrOfNotes.push({
-  name:note.name,
-  content:note.content
- });   
+    
+      this.arrOfNotes.push({
+        name:note.name,
+        content:note.content,
+       
+            });   
+     
  }
+
 
 ngDoCheck(){
   this.numberOfNotes = this.arrOfNotes.length
@@ -35,11 +42,14 @@ ngDoCheck(){
 deleteLastNote(){
   this.arrOfNotes.splice(this.arrOfNotes.length-1)
 }
- 
- 
 
 
- deleteNote(){
+
+ deleteNote(index){
+   index= this.arrOfNotes.indexOf(index);
+  this.arrOfNotes.splice(index,1);
+ 
+ 
 
  }
  
